fd5f00c47bd57bcd5fa8ea3a2386cd029bc53615
macroxu <macroxu@qq.com>
2021-07-29 09:29:27 +0800

    Update main.py

--- crawler/main.py
+++ crawler/main.py
@@ -1,13 +1,32 @@
 print('crawler begining!')
 
 fundCompanyList='http://fund.eastmoney.com/Company/default.html'
+fundCompanyBaseUrl='http://fund.eastmoney.com'
 
 from urllib import request
 from bs4 import BeautifulSoup 
 from bs4.element import Tag 
 
-###解析
-def parseHtmlPage( str ):
+
+
+###解析基金公司详细信息
+def parseFundCompanyDetailHtmlPage(url,name):
+    fullFundCompanyDetailUrl=fundCompanyBaseUrl+url
+    print(fullFundCompanyDetailUrl)
+    with request.urlopen(fullFundCompanyDetailUrl) as f:
+        data = f.read()
+        print('Status:', f.status, f.reason)
+        
+        print('Data:', data.decode('utf-8'))
+
+        print(fullFundCompanyDetailUrl)
+
+    return
+
+
+
+###解析基金公司列表信息
+def parseFundCompanyListHtmlPage( str ):
     #print (str)
     soup = BeautifulSoup(str, 'html.parser')
 
@@ -18,9 +37,11 @@ def parseHtmlPage( str ):
         if(type(item)==Tag):
             print(item)
             if(item.name=="a"):
-                print(item.attrs['href'])
-                print(item.string)
-        
+                href=item.attrs['href']
+                companyName=item.string
+                parseFundCompanyDetailHtmlPage(href,companyName)
+                return;
+
         """
         href=item.attrs['href']
         content=item.string
@@ -29,11 +50,12 @@ def parseHtmlPage( str ):
     return
 
 
+###获取基金公司列表
 with request.urlopen(fundCompanyList) as f:
     data = f.read()
     print('Status:', f.status, f.reason)
     for k, v in f.getheaders():
         print('%s: %s' % (k, v))
     print('Data:', data.decode('utf-8'))
-    parseHtmlPage(data.decode('utf-8'))
+    parseFundCompanyListHtmlPage(data.decode('utf-8'))
 
